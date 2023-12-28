928a620441827e676ac3eed41a9e8626a997652a
tonytor <teme4842@gmail.com>
2023-12-28 17:37:20 +0300

    if the player shoot the laser and  hit the enemy, destroy the enemy

--- Projectile/enemy.gd
+++ Projectile/enemy.gd
@@ -3,3 +3,10 @@ extends Area2D
 var speed = 200
 func _process(delta):
 	position.y+=speed*delta
+
+
+
+
+
+func _on_area_entered(_area):
+	self.queue_free()
--- Projectile/laser.gd
+++ Projectile/laser.gd
@@ -3,3 +3,10 @@ extends Area2D
 const speed = 200
 func _process(delta):
 	position.y-=speed*delta
+
+
+
+
+
+func _on_area_entered(_area):
+	self.queue_free()
