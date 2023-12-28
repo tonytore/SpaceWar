a3ddfe494f9ad9edf6baa901c4cea397c839b5e0
tonytor <teme4842@gmail.com>
2023-12-28 17:22:38 +0300

    make player fire laser

--- Player/player.gd
+++ Player/player.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _process(delta):
 	var mouse_pos = get_global_mouse_position()
 	position.x = mouse_pos.x
-#	if Input.is_action_pressed("fire"):
-#		var new_laser = laser_scene.instantiate()
-#		add_child(new_laser)
-##		new_laser.position = self.position
+	if Input.is_action_pressed("fire"):
+		var new_laser = laser_scene.instantiate()
+		add_sibling(new_laser)
+		new_laser.position = self.position
